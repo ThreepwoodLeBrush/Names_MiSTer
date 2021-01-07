cfa256c2406dfac86e2faf975a1a061e7e7b1022
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-01-07 21:51:01 +0100

    Added workflow.

---
+++ .github/regenerate_files.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Copyright (c) 2021 José Manuel Barroso Galindo <theypsilon@gmail.com>
+
+set -euo pipefail
+
+export GIT_MERGE_AUTOEDIT=no
+git config --global user.email "theypsilon@gmail.com"
+git config --global user.name "The CI/CD Bot"
+
+echo "Regenerating Names TXT files:"
+echo
+./scripts/generate_names_txt_files.py
+echo
+echo
+
+echo "Regenerating Names CSV:"
+echo
+./scripts/generate_names_txt_files.py
+echo
+echo
+
+git add names*
+
+if ! git diff --staged --quiet --exit-code --ignore-space-at-eol ; then
+    echo "There are changes to commit."
+    echo
+    git commit -m "BOT: Regenerated files."
+    git push origin master
+    echo
+    echo "New files deployed."
+else
+    echo "Nothing to be updated."
+fi
